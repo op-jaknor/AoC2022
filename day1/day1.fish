940578db7cb2e81e5698dfe195c80f58dd872dbb
Jakob Nordstrom <Jakob.Nordstrom@omegapoint.se>
2022-12-01 08:32:43 +0100

    fix small error in fish file

--- day1/day1.fish
+++ day1/day1.fish
@@ -3,8 +3,7 @@ if($0>0)
 CURR+=$0
 else
 {print CURR
-CURR = 0}}'
- input.txt | sort -g | tail -3 | awk '{S+=$0} END {print S}'
+CURR = 0}}'  input.txt | sort -g | tail -3 | awk '{S+=$0} END {print S}'
 
 
 
